9395479078b4c9662e9e88dbfe5c19d2f4d7dbd0
Olesia Erastova <olesia1205@yandex.ru>
2024-09-07 14:49:15 +0300

    add footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,17 @@
 import './index.css';
+
+document.addEventListener('DOMContentLoaded', function() {
+  const runningLine = document.querySelector('.running-line');
+  const footerText = document.querySelector('.footer__text');
+  const footer = document.querySelector('.footer');
+
+  if (runningLine && footerText && footer) {
+    const clonedRunningLine = runningLine.cloneNode(true);
+    footer.insertBefore(clonedRunningLine, footerText);
+    clonedRunningLine.classList.add('footer-line');
+  } else {
+    console.error('One of the elements was not found')
+  }
+});
+
+
