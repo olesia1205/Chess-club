2a4f89d9ad7fd474abe32937f0456c45178ee2f8
Olesia Erastova <olesia1205@yandex.ru>
2024-09-08 19:16:08 +0300

    add stages section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,25 @@
 import './index.css';
+import { cardData } from '../assets/constants';
+
+document.addEventListener('DOMContentLoaded', function() {
+  const stagesList = document.getElementById('stagesList');
+  const cardTemplate = document.getElementById('cardTemplate').content;
+
+  cardData.forEach(card => {
+    const cardClone = document.importNode(cardTemplate, true);
+    const numberElement = cardClone.querySelector('.stages__card-number');
+    const descriptionElement = cardClone.querySelector('.stages__card-description');
+
+    numberElement.textContent = `${card.number}`;
+    descriptionElement.textContent = card.description;
+    stagesList.appendChild(cardClone);
+  });
+
+  const cardsArr = document.querySelectorAll('.stages__card');
+  cardsArr.forEach((el, index) => {
+    el.classList.add(`stages__card${index + 1}`);
+  });
+});
 
 document.addEventListener('DOMContentLoaded', function() {
   const runningLine = document.querySelector('.running-line');
