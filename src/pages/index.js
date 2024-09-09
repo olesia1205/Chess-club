2708814b94f6210f1c057da45f65d49aa9a41660
Olesia Erastova <olesia1205@yandex.ru>
2024-09-09 13:07:59 +0300

    add adaptiv for stages & pagination

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import './index.css';
 import { cardData } from '../assets/constants';
 
+// Отрисовка карточек в секции stages для десктопа
 document.addEventListener('DOMContentLoaded', function() {
   const stagesList = document.getElementById('stagesList');
   const cardTemplate = document.getElementById('cardTemplate').content;
@@ -21,6 +22,64 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 });
 
+// Отрисовка карточек в секции stages для мобильной версии, логика работы пагинатора
+document.addEventListener('DOMContentLoaded', function() {
+  const cards = document.querySelectorAll('.stages__card-mobile');
+  const prevButton = document.querySelector('.pagination__button-prev');
+  const nextButton = document.querySelector('.pagination__button-next');
+  const pointsContainer = document.querySelector('.pagination__points');
+
+  let currentIndex = 0;
+
+  function createPaginationPoints() {
+    cards.forEach((_, index) => {
+      const point = document.createElement('div');
+      point.classList.add('pagination__point');
+      if (index === currentIndex) {
+        point.classList.add('active');
+      }
+      pointsContainer.appendChild(point);
+    });
+  };
+
+  function updateCards() {
+    cards.forEach((card, index) => {
+      card.classList.remove('active');
+    });
+    cards[currentIndex].classList.add('active');
+
+    const points = document.querySelectorAll('.pagination__point');
+    points.forEach((point, index) => {
+      point.classList.remove('active');
+      if (index === currentIndex) {
+        point.classList.add('active');
+      }
+    });
+  };
+
+  prevButton.addEventListener('click', () => {
+    if (currentIndex > 0) {
+      currentIndex--;
+    } else {
+      currentIndex = cards.length - 1;
+    }
+    updateCards();
+  });
+
+  nextButton.addEventListener('click', () => {
+    if (currentIndex < cards.length - 1) {
+      currentIndex++;
+    } else {
+      currentIndex = 0;
+    }
+    updateCards();
+  });
+
+  createPaginationPoints();
+  updateCards();
+});
+
+// Добавление бегущей строки в подвале
 document.addEventListener('DOMContentLoaded', function() {
   const runningLine = document.querySelector('.running-line');
   const footerText = document.querySelector('.footer__text');
@@ -34,5 +93,3 @@ document.addEventListener('DOMContentLoaded', function() {
     console.error('One of the elements was not found')
   }
 });
-
-
